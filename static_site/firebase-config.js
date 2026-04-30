b070ebcf5174700012bc731fc28a0a632668ead3
ZAIN ALI <97796695+mrZAIN-ALI@users.noreply.github.com>
2026-05-01 01:52:19 +0500

    Enable Firebase Google sign in

--- static_site/firebase-config.js
+++ static_site/firebase-config.js
@@ -1,9 +1,12 @@
 window.ROAD_SIGN_FIREBASE_CONFIG = {
-  enabled: false,
+  enabled: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    appId: ""
+    apiKey: "AIzaSyAwYFjQa2bmMEOE4cyrITjUDfp9FgXhmWQ",
+    authDomain: "drivingtestpractise.firebaseapp.com",
+    projectId: "drivingtestpractise",
+    storageBucket: "drivingtestpractise.firebasestorage.app",
+    messagingSenderId: "780985399562",
+    appId: "1:780985399562:web:44f50027ffd115bcd6a2f3",
+    measurementId: "G-R7C5F1M2V5"
   }
 };
